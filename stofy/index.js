59783ce8c6c366b7fb605ea5814041e71d0d67f2
nathiyadev <nathiyadev209@gmail.com>
2024-03-21 09:17:14 +0530

    add e-commerce

---
+++ stofy/index.js
@@ -0,0 +1,10 @@
+var sidenav = document.querySelector(".side-navbar")
+
+function showNavbar()
+{
+    sidenav.style.display="block"
+}
+function closeNavbar()
+{
+    sidenav.style.display="none"
+}
